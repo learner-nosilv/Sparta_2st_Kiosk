03de97d3a0dc1426f1f0b7a75a5cf94711473874
Nosilv <nosilv8@gmail.com>
2025-01-21 11:13:04 +0900

    setup: 클래스 정의 파일 생성
    resolves: #3

---
+++ src/main/java/Kiosk.java
@@ -0,0 +1,6 @@
+/*
+Kiosk: 프로그램 순서 및 흐름 제어를 담당하는 클래스
+*/
+
+public class Kiosk {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/*
+Main : 프로그램의 시작 지점이 되는 클래스
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
