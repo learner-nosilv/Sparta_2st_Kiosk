c1736ecf0d47d39c27171f48b11e91716f4bf0ef
Nosilv <nosilv8@gmail.com>
2025-01-22 02:16:44 +0900

    feat: Menu class정의(아이템리스트관리)
    resolves: #18

--- src/main/java/com/example/kiosk_1/Menu.java
+++ src/main/java/com/example/kiosk_1/Menu.java
@@ -4,6 +4,20 @@ Menu: MenuItem 클래스를 관리하는 클래스
 */
 package com.example.kiosk_1;
 
-public class Menu {
+import java.util.List;
 
+public class Menu {
+    // Attributes: 카테고리이름, 카테고리에 속하는 MenuItem 객체의 List
+    private String category;
+    private List<MenuItem> categoryItems;
+
+    // 생성자
+    public Menu(String category, List<MenuItem> categoryItems) {
+        this.category = category;
+        this.categoryItems = categoryItems;
+    }
+
+    // Getter Method - Encapsulation
+    public String getCategory() { return this.category; }
+    public List<MenuItem> getCategoryItems() { return this.categoryItems; }
 }
